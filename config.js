a8432388bf0e87ef27ce6c4c16f7699c971f1054
Lonny Grafman <lonny@appropedia.org>
2025-08-31 22:55:01 -0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+// Global config for Portfolio
+// Override at runtime with ?csv=URL
+window.PORTFOLIO_CONFIG = {
+  CSV_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRwYDBqR1Ig7T0gzs172Esm_cpJ28o8JwClwiiLtXQ65Fjd4_aVY13nKR3p_F6Qtpjq76p9yDYpsmFl/pub?output=csv"
+};
